53f1572ba2004ec96be0fe49bd3a5d4c7f69a87f
Jose Angel Fosado Animas <jfosadoanimas@gmail.com>
2022-10-13 16:49:15 -0500

    Se termino de mostrar las principales caracteristicas de svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
 	import Skills from "./components/Skills.svelte";
 	import Input from "./components/Input.svelte";
 	import Photos from "./components/Photos.svelte";
+	import ButtonCount from "./components/ButtonCount.svelte";
 
 	export let name;
 	export let lastName;
@@ -25,6 +26,7 @@
 	<Person {...data}/>
 	<Skills />
 	<Input />
+	<ButtonCount />
 	<Photos />
 	<img src={svelteLogo} alt="Logo de Svelte">
 </main>
